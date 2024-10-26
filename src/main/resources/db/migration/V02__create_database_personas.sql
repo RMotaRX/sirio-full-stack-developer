54d68651f75221f6b77ab819a1299508d800d529
Rafael Mota <rmorixa@rmrx.local>
2024-10-26 17:48:03 -0300

    [SFSD_0001] - initial configurarion.

---
+++ src/main/resources/db/migration/V02__create_database_personas.sql
@@ -0,0 +1,18 @@
+CREATE TABLE persona(
+  id BIGINT(36) NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  cpf VARCHAR(11) NOT NULL UNIQUE,
+  celular VARCHAR(20) NOT NULL UNIQUE,
+  logradouro VARCHAR(50),
+  numero VARCHAR(6),
+  complemento VARCHAR(20),
+  bairro VARCHAR(20),
+  cep VARCHAR(20),
+  cidade VARCHAR(50),
+  estado VARCHAR(2),
+  status VARCHAR(8) NOT NULL,
+  data_criacao DATE,
+  data_remocao DATE,
+  id_usuario BIGINT(36) NOT NULL
+
+);
